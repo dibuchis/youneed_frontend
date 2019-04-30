0a89ec745595e40af446d44bbe6faec2ac6b52cb
dibuchis <dibuchis@gmail.com>
2019-04-30 12:10:23 -0500

    Change Server Config

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *'); 
-require __DIR__ . '../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 use Youneed\App;
 use Youneed\Controllers\User;
