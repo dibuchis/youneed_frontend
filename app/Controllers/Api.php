bcff5b51013681b8bb38730c2e53693ee2492bf0
dibuchis <dibuchis@gmail.com>
2019-05-27 14:35:14 -0500

    Cambios de implementación varios

--- app/Controllers/Api.php
+++ app/Controllers/Api.php
@@ -145,4 +145,22 @@ function contratarAsociado(){
 	}
 }
 
+function cargarNotificaciones($id){
+    $nots = null;
+    $ch = curl_init();
+    
+    curl_setopt($ch, CURLOPT_URL, 'https://app.youneed.com.ec/ajax/getnotificaciones?uid=' . $id);
+    
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //Return data instead printing directly in Browser
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10); //Timeout after 7 seconds
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)");
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    
+    $res = curl_exec($ch);
+    
+    curl_close($ch);
+    
+    return json_decode($res);
+}
+
 }
--- views/_dashboard.php
+++ views/_dashboard.php
@@ -7,6 +7,7 @@
 <?php 
 
 $res = $api->cargarPedidos($user->id);
+$nots = $api->cargarNotificaciones($user->id);
 
 ?>
 
@@ -14,6 +15,7 @@ $res = $api->cargarPedidos($user->id);
 
 <div class="row">
     <div class="dashboard-panel col-md-6">
+        <div class="header"><center>Pedidos</center></div>
         <div class="dashboard-panel-wrapper">
             <?php if(count($res)){; ?>
 				<div class="row row m-0 p-0">
@@ -21,7 +23,7 @@ $res = $api->cargarPedidos($user->id);
 					<div class="col-xs-5 col-md-4 dashboard-panel-header">Fecha de creación</div>
 					<div class="col-xs-5 col-md-6 dashboard-panel-header">Cliente</div>
 				</div>
-                <div class="dashboard-panel-content-item">
+            <div class="dashboard-panel-content-item">
 					<?php
 					foreach($res->pedidos as $key => $val){
 						echo '<div class="row notif_row">';
@@ -37,7 +39,7 @@ $res = $api->cargarPedidos($user->id);
 						echo '</div>';
 					}
 					?>
-				<?php }else{ ?>
+			<?php }else{ ?>
 				<div class="dashboard-panel-header">Pedidos</div>
 					<div class="dashboard-panel-content">
 						<i>Por el momento no tiene ningún pedido.</i>
@@ -45,14 +47,50 @@ $res = $api->cargarPedidos($user->id);
                 <?php }; ?>
             </div>
             <div class="dashboard-panel-footer">
-                Ver todos los pedidos
+            <?php if(count($res) > 10){ echo "<a href='views/_pedidos'>Ver todos los pedidos.</a>"; } ?>
             </div>
         </div>
     </div>
 
     <div class="dashboard-panel col-md-6">
+        <div class="header"><center>Notificaciones</center></div>
         <div class="dashboard-panel-wrapper">
-            <div class="dashboard-panel-header">Historial</div>
+            <div class="dashboard-panel-content">
+                <?php if(count($nots)){; ?>
+                    <div class="row row m-0 p-0">
+                        <div class="col-xs-2 col-md-1 dashboard-panel-header">ID</div>
+                        <div class="col-xs-6 col-md-11 dashboard-panel-header">Mensaje</div>
+                    </div>
+                <div class="dashboard-panel-content-item">
+                        <?php
+                            foreach($nots->notificaciones as $key => $val){
+                                echo '<div class="row notif_row">';
+                                    echo '<div class="col-xs-2 col-md-1 notif_id">';
+                                    echo	$val->id;
+                                    echo '</div>';
+                                    echo '<div class="col-xs-6 col-md-11 notif_id">';
+                                    echo	$tipo_notificacion[$val->tipo_notificacion_id];
+                                    echo '</div>';
+                                echo '</div>';
+                            }
+                        ?>
+                <?php }else{ ?>
+                    <div class="dashboard-panel-header">Notificaciones</div>
+                        <div class="dashboard-panel-content">
+                            <i>Por el momento no tiene ningúna notificacion.</i>
+                        </div>
+                    <?php }; ?>
+                </div>
+            </div>
+            <div class="dashboard-panel-footer">
+                <?php if(count($res) > 10){ echo "<a href='views/_notificaciones'>Ver todas las notificaciones.</a>"; } ?>
+            </div>
+        </div>
+    </div>
+    
+    <div class="dashboard-panel col-md-6">
+        <div class="dashboard-panel-wrapper">
+            <div class="dashboard-panel-header">Historial de pedidos</div>
             <div class="dashboard-panel-content">
                 <div class="dashboard-panel-content-item">
                 <i>Por el momento no tiene ningun registro.</i>
@@ -63,6 +101,7 @@ $res = $api->cargarPedidos($user->id);
             </div>
         </div>
     </div>
+
     
 
 </div>
