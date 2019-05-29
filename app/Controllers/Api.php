551bcf30c80e2957270b33b99806d2082dcdcc03
dibuchis <dibuchis@gmail.com>
2019-05-29 16:04:35 -0500

    Contratar Asociado

--- app/Controllers/Api.php
+++ app/Controllers/Api.php
@@ -109,7 +109,7 @@ function contratarAsociado(){
 
     if($app->isAjaxRequest()){
 
-    if(isset($_POST['Pedido'])){
+    if(isset($_POST['data'])){
 	}
 			// $query = "?servicio_id=" . $_POST['servicio_id'] . "&";
 			// $query .= "asociado_id=" . $_POST['asociado_id'] . "&";
