a804afaa786dc6d5cf350f6d798d6a8071518609
dibuchis <dibuchis@gmail.com>
2019-05-30 11:17:40 -0500

    Cambios Página Pedidos

--- views/_success.php
+++ views/_success.php
@@ -1,55 +1,113 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    
-
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8.8.7/dist/sweetalert2.all.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@8.8.7/dist/sweetalert2.min.css">
-
-
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="https://youneed.com.ec/wp-content/themes/Avada-Child-Theme/style.css">
-    
-    <link rel="stylesheet" href="https://app.youneed.com.ec/css/admin.css">
-
-	<style>
-	</style>
-    <script type="text/javascript" src="<?php echo $app::APP_BASEURL . 'js/main.js'; ?>"></script>
-</head>
-    <body>
-
-    <div class="container" id="form-registro-cliente">
-        <form id="client-register-form">
-        <div class="panel panel-primary setup-content panel-default">
-	            <div class="panel-heading">
-	                 <h3 class="panel-title">Estado de Solicitud</h3>
-	            </div>
-	            <div class="panel-body">
-	            	<div class="row">
-						<div class="col-md-12">
-	                    <h4>Felicitaciones! </h4>
-                        <hr>
-                        
-                        <div class="col-md-12">
-							<p>Tu solicitud de servicio a sido enviada exitosamente!</p>
-							<p>Muy pronto se comunicarán con tigo.</p>
-						</div>
-                        </div>
-                    </div>
-                </div>
-        </div>
-    </div>
-    
-    </body>
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+    
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+    
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8.8.7/dist/sweetalert2.all.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@8.8.7/dist/sweetalert2.min.css">
+
+
+
+
+
+    <!-- Latest compiled and minified CSS -->
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+
+    <!-- Optional theme -->
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+
+
+    <link rel="stylesheet" href="https://youneed.com.ec/wp-content/themes/Avada-Child-Theme/style.css">
+
+    
+
+    <link rel="stylesheet" href="https://app.youneed.com.ec/css/admin.css">
+
+
+
+	<style>
+
+	</style>
+
+    <script type="text/javascript" src="<?php echo $app::APP_BASEURL . 'js/main.js'; ?>"></script>
+
+</head>
+
+    <body>
+
+
+
+    <div class="container" id="form-registro-cliente">
+
+        <form id="client-register-form">
+
+        <div class="panel panel-primary setup-content panel-default">
+
+	            <div class="panel-heading">
+
+	                 <h3 class="panel-title">Estado de Solicitud</h3>
+
+	            </div>
+
+	            <div class="panel-body">
+
+	            	<div class="row">
+
+						<div class="col-md-8 col-md-offset-2">
+
+	                    <center><h3>Felicitaciones! </h3></center>
+
+                        <hr>
+
+                        
+
+                        <div class="col-md-12">
+
+							<center><p>Tu solicitud de servicio a sido enviada exitosamente!</p>
+
+                            <p>Revisa tu correo electrónico</p>
+
+							<p>Muy pronto se comunicarán con tigo.</p>
+
+                            <p><a class="btn btn-success" href="https://youneed.com.ec/app/dashboard.php">Volver al perfil</a></p></center>
+
+						</div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+        </div>
+
+    </div>
+
+    
+
+    </body>
+
 </html>
