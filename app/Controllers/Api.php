01284d8b1b6ed2db3d8cd5d70b9ed90b1f2421ae
dibuchis <dibuchis@gmail.com>
2019-06-06 15:49:56 -0500

    Aceptar, Cancelar Pedido

--- app/Controllers/Api.php
+++ app/Controllers/Api.php
@@ -116,11 +116,9 @@ function aceptarPedido($id){
         curl_close($ch);
         
         echo json_decode($response);
-        exit();
 
     }else{
         echo json_encode($response);
-        exit();
     }
 }
 
@@ -160,10 +158,9 @@ function cancelarPedido($id){
         curl_close($ch);
         
         echo json_decode($response);
-        exit();
+
     }else{
         echo json_encode($response);
-        exit();
     }
 }
 
@@ -207,10 +204,8 @@ function contratarAsociado(){
             $response = json_encode($dataRes);
         
             echo $dataRes;
-            exit();
 	}else{
-        echo json_encode($response);
-        exit();
+		echo json_encode($response);
 	}
 }
 
@@ -255,7 +250,6 @@ function cargarNotificaciones($id){
 
     }else{
         echo json_encode($response);
-        exit();
     }
 }
 
@@ -300,7 +294,6 @@ function cargarPedidos($id){
 
     }else{
         echo json_encode($response);
-        exit();
     }
 }
 
